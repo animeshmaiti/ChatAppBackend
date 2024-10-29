71b29a620acdfd1d9770781c13f9ad9febf40a90
Animesh Maiti <a.maiti770@gmail.com>
2024-10-30 00:06:44 +0530

    debug

--- middleware/protectRoute.js
+++ middleware/protectRoute.js
@@ -4,6 +4,7 @@ import User from '../models/user.js';
 const protectRoute = async(req, res, next) => {
   try {
     const token = req.cookies.token;
+    console.log('Token: ', token);
     if (!token) {
       return res.status(401).json({ error: 'Unauthorized' });
     }
